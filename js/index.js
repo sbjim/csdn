2c09f340def860ce6b13e7057235d3d0f76d0c46
Jim <1255905516@qq.com>
2020-09-27 11:26:13 +0800

    去掉 关注博主即可阅读全文

--- js/index.js
+++ js/index.js
@@ -1,13 +1,16 @@
 $(function() {
 
 	// 显示文章的内容
-	
-	
-	
+
+	// 关注博主即可阅读全文
+	$("#article_content").css('height','100%');
+	$('.hide-article-box.hide-article-pos.text-center').remove()
+
 	// 显示文章的评论
 	$("div[class=comment-list-box]").css('max-height','100%')
 	$("div[id=btnMoreComment]").remove()
 
+	//
 	// 去广告
 	$("div[class=advert]").remove() 
 	$("div[class=banner-ad-box]").remove() 
@@ -15,8 +18,9 @@ $(function() {
 	
 	$(".slide-outer.right_top").remove()
 	$(".advert").remove()
-	$('.right_extension.slide-outer').remove() 
-	
+	$('.right_extension.slide-outer').remove()
+
+
 	$('#footerRightAds').remove()  
 	$('#recommendAdBox').remove()  
 	
